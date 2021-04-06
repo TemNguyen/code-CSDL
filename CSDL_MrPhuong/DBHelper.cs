de66d8a8897f2945d556d9dc10e0ee6006706fd6
Nguyễn Duy Thịnh <temthinh30012001@gmail.com>
2021-04-06 20:28:46 +0700

    first commit

---
+++ CSDL_MrPhuong/DBHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDL_MrPhuong
+{
+    class DBHelper
+    {
+        private SqlConnection conn;
+        public static DBHelper Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    string conns = @"Data Source=DESKTOP-1GG0LVP;Initial Catalog=QLSV;Integrated Security=True";
+                    _Instance = new DBHelper(conns);
+                }
+                return _Instance;
+            }
+            private set { }
+        }
+        private static DBHelper _Instance;
+        private DBHelper(string s)
+        {
+            conn = new SqlConnection(s);
+        }
+        public DataTable GetRecords(string _query)
+        {
+            string query = _query;
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            conn.Open();
+            da.Fill(dt);
+            conn.Close();
+            return dt;
+        }
+        public void ExcutedDB(string sql)
+        {
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
---
+++ CSDL_MrPhuong/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSDL_MrPhuong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showDataSet();
+        }
+        private void Show()
+        {
+            string connstring = @"Data Source=DESKTOP-1GG0LVP;Initial Catalog=QLSV;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connstring);
+            string query = textBox1.Text;
+            SqlCommand cmd = new SqlCommand(query, conn);
+            DataTable dt = new DataTable();
+            dt.Columns.AddRange(new DataColumn[]
+                {
+                    new DataColumn("MSSV"),
+                    new DataColumn("NameSV"),
+                    new DataColumn("Gender", typeof(bool)),
+                    new DataColumn("NS", typeof(DateTime)),
+                    new DataColumn("ID_Lop", typeof(int))
+            });
+            conn.Open();
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                DataRow dr = dt.NewRow();
+                dr["MSSV"] = r["MSSV"];
+                dr["NameSV"] = r["NameSV"];
+                dr["Gender"] = r["Gender"];
+                dr["NS"] = r["NS"];
+                dr["ID_Lop"] = r["ID_Lop"];
+                dt.Rows.Add(dr);
+            }
+            //textBox1.Text = ((int)(cmd.ExecuteScalar())).ToString();
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+        public void showDataSet()
+        {
+            string connstring = @"Data Source=DESKTOP-1GG0LVP;Initial Catalog=QLSV;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(connstring);
+            string query = "select * from SV where MSSV = @M";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            cmd.Parameters.Add("@M", SqlDbType.NVarChar);
+            cmd.Parameters["@M"].Value = textBox1.Text;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            conn.Open();
+            da.Fill(ds);
+            conn.Close();
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
